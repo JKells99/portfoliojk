a1dceafbc05483c138d0a683c1322c5571dddd07
Jordan <jordan.kelloway@keyin.com>
2023-08-30 17:09:15 -0230

    chNGE 5

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter ,Route, Routes } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename='/portfoliojk'>
+      <BrowserRouter>
     <Routes>
         <Route path='/*' element={<App />}/>
       </Routes>
